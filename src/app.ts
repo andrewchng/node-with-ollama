78eaf8ff4d4c7050c17bcd2903a1131c98800bd9
andrew.chng <accen-andrewchng@gcc.gov.sg>
2025-02-03 14:19:45 +0800

    added example for langchain

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,40 @@
-import ollama from 'ollama';
+import ollama from "ollama";
+import { ChatOllama } from "@langchain/ollama";
 
+const modelName = "llama3.2:latest";
+
+// using basic ollama library to run LLM locally
 const main = async () => {
   try {
-    const model = "llama3.2:latest";
     const prompt = "What is the capital of France?";
-    
+
     console.log("Prompting for a response...");
     console.log(prompt);
     const response = await ollama.chat({
-      model: model, // or any model name you've downloaded with Ollama
-      messages: [{ role: 'user', content: prompt }],
+      model: modelName, // or any model name you've downloaded with Ollama
+      messages: [{ role: "user", content: prompt }],
     });
     console.log(response.message.content); // Output: Paris
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error("An error occurred:", error);
   }
 };
 
+// Using Langchain to run ollama's LLM locally
+const langchain = async () => {
+  try {
+    const model = new ChatOllama({ model: modelName });
+
+    const prompt = "Explain quantum mechanics in simple terms.";
+    console.log("Prompting for a response...");
+    console.log(prompt);
+
+    const res = await model.invoke(prompt);
+    console.log(res.content);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+};
+
+// langchain();
 main();
